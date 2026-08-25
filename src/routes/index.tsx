2d783c217badcef812b811bd2a2baf97dd6b9969
sajagshrestha <sajagshrestha0852@gmail.com>
2026-08-25 07:51:59 +0545

    Init TanStack Start app with SSR disabled

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({ component: App })
+
+function App() {
+  return (
+    <main className="grid min-h-screen place-items-center">
+      <h1 className="text-4xl font-bold tracking-tight">Hello World</h1>
+    </main>
+  )
+}
